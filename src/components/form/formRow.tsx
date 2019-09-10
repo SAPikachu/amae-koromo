6a7187aa878a504ead75d1d2d5700bee53097cc7
Joe Hu <i@sapika.ch>
2019-09-10 13:19:06 +0800

    Fix build

--- src/components/form/formRow.tsx
+++ src/components/form/formRow.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-export const FormRow = ({ title = "", children }) => (
+export const FormRow = ({ title = "", children = undefined as ReactNode }) => (
   <div className="form-group row">
     <label className="col-sm-2 col-form-label">{title}</label>
     <div className="col-sm-10">{children}</div>
