4528dd01073ad08dbe86c4871afee90659fb2ba2
SAPikachu <sapikachu@gmail.com>
2019-10-29 14:21:29 +0800

    Optimize CSS
    * Remove unnecessary parts of Bootstrap
    * Merge all CSS files to single file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-virtualized/styles.css";
-import "./styles.css";
+import "./styles/bootstrap-custom.scss";
+import "./styles/styles.scss";
 
 import App from "./components/app";
 
