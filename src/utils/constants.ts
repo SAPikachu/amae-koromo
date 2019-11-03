3c06772c04accec244a4789f8bb360a32369bb28
SAPikachu <sapikachu@gmail.com>
2019-11-03 08:07:01 +0800

    Change domain name of API server

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 
-export const DATA_ROOT = "https://d2ek5tscv4zquv.cloudfront.net/";
+export const DATA_ROOT = "https://ak-data-1.sapk.ch/";
 export const API_ROOT = `${DATA_ROOT}api/`;
 export const TITLE_PREFIX = "雀魂牌谱屋";
 export const DATE_MIN = dayjs("2019-08-23", "YYYY-MM-DD");
