31976cf4192d19fca8a07e753f938278fc663ef7
SAPikachu <sapikachu@gmail.com>
2019-11-03 19:11:19 +0800

    Skip maintenance for local development

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 
 export const DATA_ROOT = "https://ak-data-1.sapk.ch/";
-export const API_ROOT = `${DATA_ROOT}api/`;
+const API_ROOT_PROD = `${DATA_ROOT}api/`;
+const API_ROOT_TEST = `${DATA_ROOT}api-test/`;
+export const API_ROOT = process.env.NODE_ENV === "development" ? API_ROOT_TEST : API_ROOT_PROD;
 export const TITLE_PREFIX = "雀魂牌谱屋";
 export const DATE_MIN = dayjs("2019-08-23", "YYYY-MM-DD");
