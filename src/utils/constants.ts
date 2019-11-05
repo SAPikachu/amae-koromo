87776e558e652d27685bf8ffb828f46de546b34c
SAPikachu <sapikachu@gmail.com>
2019-11-05 09:12:06 +0800

    Switch to Azure CDN

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 
-export const DATA_ROOT = "https://ak-data-1.sapk.ch/";
+export const DATA_ROOT = "https://ak-data-2.sapk.ch/";
 const API_ROOT_PROD = `${DATA_ROOT}api/`;
 const API_ROOT_TEST = `${DATA_ROOT}api-test/`;
 export const API_ROOT = process.env.NODE_ENV === "development" ? API_ROOT_TEST : API_ROOT_PROD;
