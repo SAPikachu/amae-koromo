b6d8954d285ce7cbb6c1d4bb352d4b30765c642e
SAPikachu <sapikachu@gmail.com>
2019-11-08 19:10:33 +0800

    Allow changing mode for ranking

--- src/components/ranking/deltaRanking.tsx
+++ src/components/ranking/deltaRanking.tsx
@@ -7,51 +7,70 @@ import { getDeltaRanking } from "../../data/source/misc";
 import Loading from "../misc/loading";
 import { generatePlayerPathById } from "../gameRecords/routes";
 import { LevelWithDelta } from "../../data/types";
+import { useState } from "react";
+import { ModeSelector } from "../gameRecords/modeSelector";
 
 function RankingTable({ rows = [] as DeltaRankingItem[] }) {
-  return <table className="table table-striped">
-    <tbody>
-      {rows.map(x => <tr key={x.id}>
-        <td><Link to={generatePlayerPathById(x.id)}>[{LevelWithDelta.getTag(x.level)}] {x.nickname}</Link></td>
-        <td className="text-right">{x.delta}</td>
-      </tr>)}
-    </tbody>
-  </table>
+  return (
+    <table className="table table-striped">
+      <tbody>
+        {rows.map(x => (
+          <tr key={x.id}>
+            <td>
+              <Link to={generatePlayerPathById(x.id)}>
+                [{LevelWithDelta.getTag(x.level)}] {x.nickname}
+              </Link>
+            </td>
+            <td className="text-right">{x.delta}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default function DeltaRanking() {
   const data1w = useAsyncFactory(() => getDeltaRanking(RankingTimeSpan.OneWeek), []);
   const data4w = useAsyncFactory(() => getDeltaRanking(RankingTimeSpan.FourWeeks), []);
-  const modeId = "0";
+  const [modeId, setModeId] = useState("");
   if (!data1w || !data4w) {
     return <Loading />;
   }
-  return <div className="row">
-    <div className="col-lg-6">
-      <h3 className="text-center">苦主榜</h3>
+  return (
+    <>
       <div className="row">
-        <div className="col-6">
-          <h4 className="text-center">一周</h4>
-          <RankingTable rows={data1w[modeId].bottom} />;
-        </div>
-        <div className="col-6">
-          <h4 className="text-center">四周</h4>
-          <RankingTable rows={data4w[modeId].bottom} />;
+        <div className="col">
+          <ModeSelector mode={modeId} onChange={setModeId} />
         </div>
       </div>
-    </div>
-    <div className="col-lg-6">
-      <h3 className="text-center">汪汪榜</h3>
       <div className="row">
-        <div className="col-6">
-          <h4 className="text-center">一周</h4>
-          <RankingTable rows={data1w[modeId].top} />;
+        <div className="col-lg-6">
+          <h3 className="text-center">苦主榜</h3>
+          <div className="row">
+            <div className="col-6">
+              <h4 className="text-center">一周</h4>
+              <RankingTable rows={data1w[modeId || "0"].bottom} />;
+            </div>
+            <div className="col-6">
+              <h4 className="text-center">四周</h4>
+              <RankingTable rows={data4w[modeId || "0"].bottom} />;
+            </div>
+          </div>
         </div>
-        <div className="col-6">
-          <h4 className="text-center">四周</h4>
-          <RankingTable rows={data4w[modeId].top} />;
+        <div className="col-lg-6">
+          <h3 className="text-center">汪汪榜</h3>
+          <div className="row">
+            <div className="col-6">
+              <h4 className="text-center">一周</h4>
+              <RankingTable rows={data1w[modeId || "0"].top} />;
+            </div>
+            <div className="col-6">
+              <h4 className="text-center">四周</h4>
+              <RankingTable rows={data4w[modeId || "0"].top} />;
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
+    </>
+  );
 }
