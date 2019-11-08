8bf61e03e73b83a0e591c44e2371b3ad2eec3133
SAPikachu <sapikachu@gmail.com>
2019-11-08 19:44:59 +0800

    Mobile toggle

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Location } from "history";
 import { Link, NavLink } from "react-router-dom";
 import { TITLE_PREFIX } from "../../utils/constants";
+import { useState } from "react";
 
 function isActive(match: any, location: Location): boolean {
   if (!match) {
@@ -11,13 +12,29 @@ function isActive(match: any, location: Location): boolean {
 }
 
 export default function Navbar() {
+  const [mobileVisible, setMobileVisible] = useState(false);
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
         <Link className="navbar-brand" to="/">
           {TITLE_PREFIX}
         </Link>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setMobileVisible(!mobileVisible)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse justify-content-end ${mobileVisible ? "" : "d-md-none"}`}
+          id="navbarNavAltMarkup"
+        >
           <div className="navbar-nav">
             <NavLink className="nav-item nav-link" activeClassName="active" to="/" isActive={isActive}>
               主页
