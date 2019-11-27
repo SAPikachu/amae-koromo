b1f3e2ee1aae2da7432f23f7ad4b3cfa656ba1e7
SAPikachu <sapikachu@gmail.com>
2019-11-27 16:19:31 +0800

    Reload as soon as new code is detected

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,13 +1,17 @@
 const { editWebpackPlugin } = require("@rescripts/utilities");
 
 module.exports = [
-  process.env.NODE_ENV === "production" ? config => editWebpackPlugin(
-    p => {
-      p.config.importWorkboxFrom = "local";
-      return p;
-    },
-    "GenerateSW",
-    config,
-  ) : x => x,
+  process.env.NODE_ENV === "production"
+    ? config =>
+        editWebpackPlugin(
+          p => {
+            p.config.importWorkboxFrom = "local";
+            p.config.skipWaiting = true;
+            return p;
+          },
+          "GenerateSW",
+          config
+        )
+    : x => x
 ];
 // vim: sts=2:sw=2:ts=2:expandtab
--- src/index.tsx
+++ src/index.tsx
@@ -10,4 +10,18 @@ import App from "./components/app";
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
 
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate(registration) {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener("statechange", event => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        if ((event?.target as any)?.state === "activated") {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+    }
+  }
+});
