2aab956bc550362de1a459a0db0dea2a3cac0c70
SAPikachu <i@sapika.ch>
2020-06-24 14:47:42 +0900

    Oops

---
+++ src/data/types/utils.ts
@@ -0,0 +1,8 @@
+import i18n from "../../i18n";
+import { RANK_LABELS } from "./constants";
+
+const t = i18n.t.bind(i18n);
+
+export function getRankLabelByIndex(index: number): string {
+  return t(RANK_LABELS[index]);
+}
