fe650b44bd2ee49b8809b2200ea1d0db7dee71d2
SAPikachu <i@sapika.ch>
2020-07-04 21:46:11 +0900

    Layout adjustment

--- src/components/playerDetails/playerDetailsSettings.tsx
+++ src/components/playerDetails/playerDetailsSettings.tsx
@@ -10,11 +10,11 @@ import Conf from "../../utils/conf";
 enum DateRangeOptions {
   All = "全部",
   Last4Weeks = "最近四周",
-  Custom = "自定义"
+  Custom = "自定义",
 }
 const DATE_RANGE_ITEMS = Object.keys(DateRangeOptions).map((key: string) => ({
   key: DateRangeOptions[key as keyof typeof DateRangeOptions],
-  label: DateRangeOptions[key as keyof typeof DateRangeOptions]
+  label: DateRangeOptions[key as keyof typeof DateRangeOptions],
 }));
 
 export default function PlayerDetailsSettings({ showLevel = false }) {
@@ -25,12 +25,6 @@ export default function PlayerDetailsSettings({ showLevel = false }) {
     }
     if (!model.startDate) {
       return DateRangeOptions.All;
-    }
-    if (
-      (!model.endDate || dayjs(model.endDate).isSame(dayjs(), "day")) &&
-      dayjs(model.startDate).isSame(dayjs().subtract(27, "day"), "day")
-    ) {
-      return DateRangeOptions.Last4Weeks;
     }
     return DateRangeOptions.Custom;
   });
@@ -51,7 +45,7 @@ export default function PlayerDetailsSettings({ showLevel = false }) {
           playerId: model.playerId,
           startDate: null,
           endDate: null,
-          selectedMode: undefined
+          selectedMode: undefined,
         });
         break;
       case DateRangeOptions.Last4Weeks:
@@ -59,7 +53,7 @@ export default function PlayerDetailsSettings({ showLevel = false }) {
           type: "player",
           playerId: model.playerId,
           startDate: dayjs().subtract(27, "day"),
-          endDate: null
+          endDate: null,
         });
         break;
       case DateRangeOptions.Custom:
@@ -67,41 +61,30 @@ export default function PlayerDetailsSettings({ showLevel = false }) {
           type: "player",
           playerId: model.playerId,
           startDate: customDateFrom,
-          endDate: customDateTo
+          endDate: customDateTo,
         });
         break;
     }
   }, [model, mode, customDateFrom, customDateTo, updateModel]);
-  const setSelectedMode = useCallback(mode => updateModel({ type: "player", selectedMode: mode }), [updateModel]);
+  const setSelectedMode = useCallback((mode) => updateModel({ type: "player", selectedMode: mode }), [updateModel]);
   const updateSearchTextFromEvent = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => updateModel({ type: "player", searchText: e.currentTarget.value }),
     [updateModel]
   );
   return (
-    <>
-      <div className="row">
-        <div className="col-6">
-          <FormRow title="时间" inline={true}>
-            <CheckboxGroup
-              type="radio"
-              selectedItemKey={mode}
-              items={DATE_RANGE_ITEMS}
-              groupKey="PlayerDetailsDateRangeSelector"
-              onChange={setMode as (x: string) => void}
-            />
-          </FormRow>
-        </div>
-        {showLevel && Conf.availableModes.length > 1 && (
-          <div className="col-6">
-            <FormRow title="等级" inline={true}>
-              <ModeSelector mode={model.selectedMode} onChange={setSelectedMode} />
-            </FormRow>
-          </div>
-        )}
-      </div>
-      {mode === DateRangeOptions.Custom ? (
-        <div className="row mt-n3">
-          <div className="col-6">
+    <div className="player-details-settings">
+      <div className="setting">
+        <FormRow title="时间" inline={true}>
+          <CheckboxGroup
+            type="radio"
+            selectedItemKey={mode}
+            items={DATE_RANGE_ITEMS}
+            groupKey="PlayerDetailsDateRangeSelector"
+            onChange={setMode as (x: string) => void}
+          />
+        </FormRow>
+        {mode === DateRangeOptions.Custom ? (
+          <div className="custom-period">
             <FormRow inline={true}>
               <DatePicker
                 min={Conf.dateMin}
@@ -112,22 +95,22 @@ export default function PlayerDetailsSettings({ showLevel = false }) {
               <DatePicker min={Conf.dateMin} onChange={setCustomDateTo} date={customDateTo} className="form-control" />
             </FormRow>
           </div>
+        ) : null}
+      </div>
+      {showLevel && Conf.availableModes.length > 1 && (
+        <div className="setting">
+          <FormRow title="等级" inline={true}>
+            <ModeSelector mode={model.selectedMode} onChange={setSelectedMode} />
+          </FormRow>
         </div>
-      ) : null}
+      )}
       {model.searchText ? (
-        <div className="row mb-3">
-          <div className="col-12">
-            <FormRow title="查找玩家">
-              <input
-                type="text"
-                className="form-control"
-                value={model.searchText}
-                onChange={updateSearchTextFromEvent}
-              />
-            </FormRow>
-          </div>
+        <div className="setting">
+          <FormRow title="查找玩家">
+            <input type="text" className="form-control" value={model.searchText} onChange={updateSearchTextFromEvent} />
+          </FormRow>
         </div>
       ) : null}
-    </>
+    </div>
   );
 }
