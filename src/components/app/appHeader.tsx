953a7a473fe824f6e2330f264f908af714d008bb
SAPikachu <sapikachu@gmail.com>
2021-01-10 15:41:55 +0800

    Fix date in notice

--- src/components/app/appHeader.tsx
+++ src/components/app/appHeader.tsx
@@ -13,7 +13,7 @@ function AlertDefault() {
         <li>本页面数据由第三方维护，不能绝对保证完整和正确，信息仅供参考，请勿用于不良用途。</li>
         <li>记录包含雀魂段位战金之间、玉之间及王座之间的牌谱。</li>
         <li>页面不是实时更新，对局一般会在结束后数分钟至数小时内出现。</li>
-        <li>对局数据收集从 2020 年 11 月 29 日开始（玉南及王座南为 2020 年 8 月 23 日），之前的对局已无法获取。</li>
+        <li>对局数据收集从 2019 年 11 月 29 日开始（玉南及王座南为 2019 年 8 月 23 日），之前的对局已无法获取。</li>
         <li>
           网站主线路会收集少量匿名浏览数据作后续改进及优化之用，如不希望被收集数据，请使用
           <a href={Conf.mirrorUrl}>镜像线路</a>。
@@ -43,7 +43,7 @@ function AlertEn() {
           Data is not updated in real-time, finished matches will show up on the site in a few minutes to a few hours.
         </li>
         <li>
-          Data collection was started from 2020-11-29 (2020-08-23 for Jade South and Throne South matches), matches
+          Data collection was started from 2019-11-29 (2019-08-23 for Jade South and Throne South matches), matches
           finished before then could no longer be retrived.
         </li>
         <li>
@@ -77,7 +77,7 @@ function AlertJa() {
         </li>
         <li>データの更新はリアルタイムではありません。対局がサイトに載るまで数分から数時間がかかります。</li>
         <li>
-          データの収集は 2020 年 11 月 29 日から（玉南と王座南は 2020 年 8 月 23
+          データの収集は 2019 年 11 月 29 日から（玉南と王座南は 2019 年 8 月 23
           日）です。収集開始以前の対局は検索できません。
         </li>
         <li>
