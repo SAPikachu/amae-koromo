970ab75b981dffc341ada6c4ffef7d10d5980028
SAPikachu <sapikachu@gmail.com>
2021-09-28 09:02:27 +0800

    Add italic font

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import "./i18n";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
