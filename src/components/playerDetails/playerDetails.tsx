64df508ed6fa22a078d7f16dd0fc251d8a2e648f
SAPikachu <sapikachu@gmail.com>
2021-10-18 16:08:54 +0800

    New metrics

--- src/components/playerDetails/playerDetails.tsx
+++ src/components/playerDetails/playerDetails.tsx
@@ -104,6 +104,7 @@ function fixMaxLevel(level: LevelWithDelta): LevelWithDelta {
 }
 
 function MoreStats({ stats, metadata }: { stats: PlayerExtendedStats; metadata: PlayerMetadata }) {
+  const { t } = useTranslation();
   return (
     <>
       <StatItem label="最高等级">
@@ -136,6 +137,18 @@ function MoreStats({ stats, metadata }: { stats: PlayerExtendedStats; metadata: 
       </StatItem>
       <StatItem label="副露后流局率" description="副露后流局次数 / 副露次数">
         {formatPercent(stats.副露后流局率 || 0)}
+      </StatItem>
+      <StatItem label="打点效率" description={`${t("和牌率")} * ${t("平均打点")}`}>
+        {Math.round(stats.和牌率 * stats.平均打点 || 0)}
+      </StatItem>
+      <StatItem label="铳点损失" description={`${t("放铳率")} * ${t("平均铳点")}`}>
+        {Math.round(stats.放铳率 * stats.平均铳点 || 0)}
+      </StatItem>
+      <StatItem
+        label="净打点效率"
+        description={`${t("和牌率")} * ${t("平均打点")} - ${t("放铳率")} * ${t("平均铳点")}`}
+      >
+        {Math.round(stats.和牌率 * stats.平均打点 || 0) - Math.round(stats.放铳率 * stats.平均铳点 || 0)}
       </StatItem>
       <StatItem label="总计局数">{stats.count}</StatItem>
     </>
