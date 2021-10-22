cd72b837799b2750776a2442940e9c1a7f4ffa27
SAPikachu <sapikachu@gmail.com>
2021-10-22 12:45:02 +0800

    Refactor

---
+++ src/bootstrap.tsx
@@ -0,0 +1,65 @@
+import { render } from "react-dom";
+import * as Sentry from "@sentry/react";
+
+import * as serviceWorker from "./serviceWorkerRegistration";
+import "./i18n";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./styles/styles.scss";
+
+import App from "./components/app";
+
+import preval from "preval.macro";
+
+if (process.env.NODE_ENV === "production") {
+  const buildDate = preval`const dayjs = require("dayjs"); dayjs.extend(require('dayjs/plugin/utc')); module.exports = dayjs.utc().format("YYYYMMDDHHmm")`;
+  Sentry.init({
+    dsn: "https://876acfa224b8425c92f9553b9c6676be@sentry.sapikachu.net/31",
+    release: buildDate + "-" + (process.env.REACT_APP_VERSION || "unknown").slice(0, 7),
+    ignoreErrors: ["this.hostIndex.push is not a function", "undefined is not an object (evaluating 't.uv')"],
+    denyUrls: [/^chrome-extension:\/\//i, /^moz-extension:\/\//i],
+  });
+}
+
+const rootElement = document.getElementById("root");
+render(
+  <Sentry.ErrorBoundary>
+    <App />
+  </Sentry.ErrorBoundary>,
+  rootElement
+);
+
+serviceWorker.register({
+  onControllerChange() {
+    window.location.reload();
+  },
+  onUpdate(registration) {
+    const waitingServiceWorker = registration.waiting || navigator.serviceWorker.controller;
+
+    if (waitingServiceWorker) {
+      if (waitingServiceWorker.state === "activated" || waitingServiceWorker.state === "activating") {
+        window.location.reload();
+        return;
+      }
+      waitingServiceWorker.addEventListener("statechange", (event) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const state = (event.target as any)?.state;
+        if (state === "activated" || state === "activating") {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+      window.location.reload();
+    }
+  },
+});
+
+const statusPageScript = document.createElement("script");
+statusPageScript.async = true;
+statusPageScript.src = "https://qltr0c2md09b.statuspage.io/embed/script.js";
+document.body.appendChild(statusPageScript);
