f93f7513b5921519b4f6a6a77ab0f868d722d398
SAPikachu <sapikachu@gmail.com>
2022-07-30 10:07:16 +0800

    Sort level table

--- src/data/source/misc.ts
+++ src/data/source/misc.ts
@@ -52,7 +52,10 @@ export async function getGlobalStatistics(modes: GameMode[]): Promise<GlobalStat
   return await apiGet<GlobalStatistics>(`global_statistics_2?mode=${modes.join(".")}`);
 }
 export async function getLevelStatistics(): Promise<LevelStatistics> {
-  return await apiGet<LevelStatistics>("level_statistics");
+  return await apiGet<LevelStatistics>("level_statistics").then(data => {
+    data.sort((a, b) => a[1] - b[1]);
+    return data;
+  });
 }
 export async function getGlobalHistogram(): Promise<GlobalHistogram> {
   return await apiGet<GlobalHistogram>("global_histogram");
